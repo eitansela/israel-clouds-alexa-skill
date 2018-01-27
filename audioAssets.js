9ec366326ee0cf23b110945a747760702c202891
Eitan Sela <eitan.sela@gmail.com>
2018-01-27 12:43:59 +0200

    First Push to git of israel-clouds-alexa-skill

---
+++ audioAssets.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// Cloud Talk - Podcasts: 
+var audioData = [{
+        'title': 'Episode 1',
+        'url': 'https://s3.amazonaws.com/israel-clouds-podcast-files/podcast_01.mp3'
+    },
+    {
+        'title': 'Episode 2',
+        'url': 'https://s3.amazonaws.com/israel-clouds-podcast-files/podcast_02.mp3'
+    },
+    {
+        'title': 'Episode 3',
+        'url': 'https://s3.amazonaws.com/israel-clouds-podcast-files/podcast_03.mp3'
+    }
+];
+
+module.exports = audioData;
